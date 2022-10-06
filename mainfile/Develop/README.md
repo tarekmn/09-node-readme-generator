355f5a5bb362d7259a33c27bb0ee5c279549a652
tarekmn <nytarek@gmail.com>
2022-10-06 16:41:11 -0500

    working README still missing badges

--- mainfile/Develop/README.md
+++ mainfile/Develop/README.md
@@ -1,2 +1,40 @@
-# tareksproject
+
+  undefined
+  # tareksproject
+
+## Description
+sdf 
+
+## Table of Contents 
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [FAQ](#questions)
+
+## Installation
+sdf 
+
+## Usage
+sdf 
+
+## License
+MIT 
+
+## How to Contribute
+sdf 
+
+## Tests
+sfd 
+
+
+## Questions
+GitHub username: [tarekmn](https://github.com/tarekmn)
+
+
+Reach me with additional questions at nytarek@gmail.com. Thank you! 
+
+  
+   
+
+
 
--- mainfile/Develop/utils/generateMarkdown.js
+++ mainfile/Develop/utils/generateMarkdown.js
@@ -1,6 +1,15 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license) {
+`
+    [![License: ${license}](https://img.shields.io/badge/License-${license}-yellow.svg)](https://opensource.org/licenses/${license})
+    `
+  } else {
+    return ''
+  }
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,7 +21,35 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}\n
+## Description
+${data.description} \n
+## Table of Contents 
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [FAQ](#questions)
+
+## Installation
+${data.install} \n
+## Usage
+${data.usage} \n
+## License
+${data.license} \n
+## How to Contribute
+${data.guidelines} \n
+## Tests
+${data.test} \n
+
+## Questions
+GitHub username: [${data.username}](https://github.com/${data.username})\n
+
+Reach me with additional questions at ${data.email}. Thank you! \n
+  
+   
+
+
 
 `;
 }
