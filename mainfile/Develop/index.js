6ba86caff8dcb662fa11d965f91e615e95788293
tarekmn <nytarek@gmail.com>
2022-10-06 14:49:12 -0500

    working prompt

--- mainfile/Develop/index.js
+++ mainfile/Develop/index.js
@@ -1,13 +1,91 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  'What is the project title?',
+  'What is your GitHub username?',
+  'What is your email address?',
+  'Please enter a description:',
+  'Please enter installation instructions:',
+  'Please enter usage information:',
+  'Please enter contribution guidelines:',
+  'Please enter test instructions',
+  'Please choose your license:',
+];
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: questions[0],
+      name: 'title'
+    },
+    {
+      type: 'input',
+      message: questions[1],
+      name: 'username'
+    },
+    {
+      type: 'input',
+      message: questions[2],
+      name: 'email'
+    },
+    {
+      type: 'input',
+      message: questions[3],
+      name: 'description'
+    },
+    {
+      type: 'input',
+      message: questions[4],
+      name: 'install'
+    },
+    {
+      type: 'input',
+      message: questions[5],
+      name: 'usage'
+    },
+    {
+      type: 'input',
+      message: questions[6],
+      name: 'guidelines'
+    },
+    {
+      type: 'input',
+      message: questions[7],
+      name: 'test'
+    },
+    {
+      type: 'checkbox',
+      message: questions[8],
+      name: 'license',
+      choices: ['MIT', 'GNU General', 'BSD2', 'Boost Software', 'The Unlicense' ]
+    },
+  ])
+  .then((data) => {
+    console.log(data.title)
+    console.log(data.test)
+    console.log(data.license)
+  })
+
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  // const filename = `${data.title.toLowerCase().split(' ').join('')}.json`
+
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+
+}
 
 // Function call to initialize app
 init();
