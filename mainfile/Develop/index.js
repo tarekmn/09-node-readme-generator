d31369624bf366301d7393654df1088d053c295e
tarekmn <nytarek@gmail.com>
2022-10-07 14:44:31 -0500

    reformating

--- mainfile/Develop/index.js
+++ mainfile/Develop/index.js
@@ -26,8 +26,8 @@ const questions = [
 function writeToFile(data) {
 
 
-  fs.writeFile('README.md', data, (err)=>
-  err ? console.log("error") : console.log('ReadMe Created!'))
+  fs.writeFile('README.md', data, (err) =>
+    err ? console.log("error") : console.log('ReadMe Created!'))
 
 
 }
@@ -36,62 +36,62 @@ function writeToFile(data) {
 // TODO: Create a function to initialize app
 function init() {
   inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: questions[0],
-      name: 'title'
-    },
-    {
-      type: 'input',
-      message: questions[1],
-      name: 'username'
-    },
-    {
-      type: 'input',
-      message: questions[2],
-      name: 'email'
-    },
-    {
-      type: 'input',
-      message: questions[3],
-      name: 'description'
-    },
-    {
-      type: 'input',
-      message: questions[4],
-      name: 'install'
-    },
-    {
-      type: 'input',
-      message: questions[5],
-      name: 'usage'
-    },
-    {
-      type: 'input',
-      message: questions[6],
-      name: 'guidelines'
-    },
-    {
-      type: 'input',
-      message: questions[7],
-      name: 'test'
-    },
-    {
-      type: 'list',
-      message: questions[8],
-      name: 'license',
-      choices: ['MIT', 'ISC', 'MPL 2.0', 'IPL 1.0', 'none' ]
-    },
-  ])
-  .then((data) => {
-    console.log(data.title)
-    console.log(data.test)
-    console.log(data.license)
-    writeToFile(generateMarkdown(data));
-  })
+    .prompt([
+      {
+        type: 'input',
+        message: questions[0],
+        name: 'title'
+      },
+      {
+        type: 'input',
+        message: questions[1],
+        name: 'username'
+      },
+      {
+        type: 'input',
+        message: questions[2],
+        name: 'email'
+      },
+      {
+        type: 'input',
+        message: questions[3],
+        name: 'description'
+      },
+      {
+        type: 'input',
+        message: questions[4],
+        name: 'install'
+      },
+      {
+        type: 'input',
+        message: questions[5],
+        name: 'usage'
+      },
+      {
+        type: 'input',
+        message: questions[6],
+        name: 'guidelines'
+      },
+      {
+        type: 'input',
+        message: questions[7],
+        name: 'test'
+      },
+      {
+        type: 'list',
+        message: questions[8],
+        name: 'license',
+        choices: ['MIT', 'ISC', 'MPL 2.0', 'IPL 1.0', 'none']
+      },
+    ])
+    .then((data) => {
+      console.log(data.title)
+      console.log(data.test)
+      console.log(data.license)
+      writeToFile(generateMarkdown(data));
+    })
 
- 
+
 
 }
 
